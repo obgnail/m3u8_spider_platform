276e2560bd66494c9fd7cc5615025c7e0ca0b87d
obgnail <he1251698542@gmail.com>
2022-12-19 16:59:58 +0800

    update example

--- m3u8_downloader.go
+++ m3u8_downloader.go
@@ -368,11 +368,7 @@ func errHandler(err error) {
 }
 
 func main() {
-	//url := "https://bf3.sbdm.cc/runtime/Aliyun/9208ddf4d3ad882f80a9fd59860798fc.m3u8"
-	//downloader := Default(url, "bocchi the rock 11.ts")
-	//errHandler(downloader.Run())
-
-	url := "https://yun.ssdm.cc/SBDM/KidouSenshiGundamSuiseinoMajo09.m3u8"
-	downloader := Default(url, "KidouSenshiGundamSuiseinoMajo09.ts")
+	url := "https://bf3.sbdm.cc/runtime/Aliyun/9208ddf4d3ad882f80a9fd59860798fc.m3u8"
+	downloader := Default(url, "bocchi the rock 11.ts")
 	errHandler(downloader.Run())
 }
