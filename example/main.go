ca4fcd7e06808076f718b0b368ef968a8c8dd5f3
obgnail <he1251698542@gmail.com>
2022-12-20 21:50:27 +0800

    mv file

---
+++ example/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/obgnail/m3u8_spider_platform/downloader"
+)
+
+func main() {
+	url := "https://bf3.sbdm.cc/runtime/Aliyun/9208ddf4d3ad882f80a9fd59860798fc.m3u8"
+	down := downloader.Default(url, "bocchi the rock 11.ts")
+	downloader.ErrHandler(down.Run())
+}
